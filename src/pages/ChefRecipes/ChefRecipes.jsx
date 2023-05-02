3012b369725ee1cbc6d8ea01db71e422b52f679e
Monir Hoshen <getmonirr@gmail.com>
2023-05-03 01:44:27 +0600

    loading set

--- src/pages/ChefRecipes/ChefRecipes.jsx
+++ src/pages/ChefRecipes/ChefRecipes.jsx
@@ -1,24 +1,44 @@
 import React from "react";
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigation } from "react-router-dom";
 import SingleChefBanner from "../../components/SingleChefBanner";
 import SingleRecipe from "../../components/SingleRecipe";
+import { Dna } from "react-loader-spinner";
 
 const ChefRecipes = () => {
   const chef = useLoaderData();
-  const { recipes,chef_name } = chef;
-  console.log(chef);
+  const { recipes, chef_name } = chef;
+
+  // for loading
+  const navigation = useNavigation();
   return (
     <div className="container mx-auto pb-20 px-5">
-      <SingleChefBanner chef={chef} />
-      <div className=" text-center space-y-3 py-8 mt-16">
-        <h2 className="font-bold text-4xl underline-offset-4 underline decoration-orange-400"> {chef_name} Recipes
-        </h2>
-      </div>
-      <div className="grid grid-cols-1 lg:grid-cols-3 pb-16 gap-6">
-        {recipes.map((recipe, index) => (
-          <SingleRecipe key={index} recipe={recipe} />
-        ))}
-      </div>
+      {navigation.state === "loading" ? (
+        <div className="mx-auto text-center container w-96">
+          <Dna
+            visible={true}
+            height="80"
+            width="80"
+            ariaLabel="dna-loading"
+            wrapperStyle={{}}
+            wrapperClass="dna-wrapper"
+          />
+        </div>
+      ) : (
+        <>
+          <SingleChefBanner chef={chef} />
+          <div className=" text-center space-y-3 py-8 mt-16">
+            <h2 className="font-bold text-4xl underline-offset-4 underline decoration-orange-400">
+              {" "}
+              {chef_name} Recipes
+            </h2>
+          </div>
+          <div className="grid grid-cols-1 lg:grid-cols-3 pb-16 gap-6">
+            {recipes.map((recipe, index) => (
+              <SingleRecipe key={index} recipe={recipe} />
+            ))}
+          </div>
+        </>
+      )}
     </div>
   );
 };
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,25 +1,43 @@
 import React from "react";
 import Hero from "./hero/Hero";
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigation } from "react-router-dom";
 import SingleChef from "../../components/SingleChef";
+import { Dna } from "react-loader-spinner";
 
 const Home = () => {
   // get data
   const chefs = useLoaderData();
-  console.log(chefs);
+
+  // loading
+  const navigation = useNavigation();
   return (
     <>
       <Hero />
       <div className="pb-20 container mx-auto">
         <div className="my-20 text-center space-y-3">
-          <h2 className="font-bold text-4xl underline-offset-4 underline decoration-orange-400">Our Chefs</h2>
+          <h2 className="font-bold text-4xl underline-offset-4 underline decoration-orange-400">
+            Our Chefs
+          </h2>
           <p>Lorem ipsum dolor sit amet consectetur.</p>
         </div>
-        <div className="grid grid-cols-3 gap-8">
-          {chefs.map((c) => (
-            <SingleChef key={c.id} chef={c} />
-          ))}
-        </div>
+        {navigation.state === "loading" ? (
+          <div className="mx-auto text-center container w-96">
+            <Dna
+              visible={true}
+              height="80"
+              width="80"
+              ariaLabel="dna-loading"
+              wrapperStyle={{}}
+              wrapperClass="dna-wrapper"
+            />
+          </div>
+        ) : (
+          <div className="grid grid-cols-3 gap-8">
+            {chefs.map((c) => (
+              <SingleChef key={c.id} chef={c} />
+            ))}
+          </div>
+        )}
       </div>
     </>
   );
