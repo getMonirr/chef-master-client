4f661a399f75543302a12cb893161f6682659edc
Monir Hoshen <getmonirr@gmail.com>
2023-05-03 11:54:18 +0600

    check react to pdf

--- src/pages/blog/Blog.jsx
+++ src/pages/blog/Blog.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import Pdf from "react-to-pdf";
+const ref = React.createRef();
 
 const Blog = () => {
-  return <div>this is blog</div>;
+  return (
+    <div>
+      <h3 ref={ref}>This is blog will be download pdf</h3>
+      <button>download</button>
+      <div className="App">
+        <Pdf targetRef={ref} filename="code-example.pdf">
+          {({ toPdf }) => <button onClick={toPdf}>Generate Pdf</button>}
+        </Pdf>
+        {/* <div>
+          <h1>Hello CodeSandbox</h1>
+          <h2>Start editing to see some magic happen!</h2>
+        </div> */}
+      </div>
+    </div>
+  );
 };
 
 export default Blog;
